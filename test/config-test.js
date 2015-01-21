fe04ee223b18c4c2bcf7b65e518866ef99c8857b
Keith Smith <ksmith.ut@gmail.com>
2015-01-21 16:06:19 -0700

    added tests for configuration loading

--- test/config-test.js
+++ test/config-test.js
@@ -2,8 +2,23 @@
 
 var expect = require('expect.js');
 
+function loadConfig() {
+  delete require.cache[require.resolve('../config')];
+  return require('../config');
+}
+
 describe('config', function () {
 
+  it('should load the configuration', function () {
+    var config = loadConfig();
+    expect(config).to.be.an(Object);
+    expect(config.get).to.be.a('function');
+  });
 
+  it('should get the required config options');
+
+  it('should verify that the ssl key file exists');
+
+  it('should verify that the ssl cert file exists');
 
 });
