fb628b77ad78f07a4a291efdb8cc00bd657b70c8
Keith Smith <ksmith.ut@gmail.com>
2015-01-21 17:26:21 -0700

    initial tests for config

--- config/index.js
+++ config/index.js
@@ -8,6 +8,7 @@
  * @requires npm:convict
  */
 
+var path    = require('path');
 var convict = require('convict');
 
 var config = convict({
@@ -29,7 +30,122 @@ var config = convict({
     default: 'development',
     env: 'NODE_ENV',
     arg: 'env'
+  },
+  /**
+   * @member {String} [db='postgres://127.0.0.1/leglislation'] - The database
+   * connection url
+   * @example
+   * config.get('db');
+   * @example
+   * $ DB=postgres://127.0.0.1/leglislation npm start
+   * @example
+   * $ node app --db postgres://127.0.0.1/leglislation
+   * @todo TODO make db name match package name
+   */
+  db: {
+    doc: 'The database connection url',
+    default: 'postgres://postgres:superuser@127.0.0.1/leglislation',
+    env: 'DB',
+    arg: 'db'
+  },
+  /**
+   * @member {String} host - The host that can be reached through the browser.
+   * Should be defined in `env.` config files.
+   * @example
+   * config.get('host')
+   */
+  host: {
+    doc: 'The user reachable host with protocol and port',
+    format: 'url',
+    default: null
+  },
+  /**
+   * @member {Number} [port=4430] - The port to bind.
+   * @example
+   * config.get('port');
+   * @example
+   * $ PORT=443 npm start
+   * @example
+   * $ node app --port 443
+   */
+  port: {
+    doc: 'The port to bind.',
+    format: 'port',
+    default: 4430,
+    env: 'PORT',
+    arg: 'port'
+  },
+  /**
+   * @member {String} [key='`pwd`/server.key'] - The path to the ssl key for
+   * ssl/tls
+   * @example
+   * config.get('key');
+   * @example
+   * $ KEY=/data/keys/key.pem npm start
+   * @example
+   * $ node app --key /data/keys/key.pem
+   */
+  key: {
+    doc: 'The path to the key.pem for ssl/tls',
+    default: path.resolve(__dirname, '..', 'server.key'),
+    env: 'KEY',
+    arg: 'key'
+  },
+  /**
+   * @member {String} [cert='`pwd`/server.crt'] - The path to the ssl
+   * certificate for ssl/tls
+   * @example
+   * config.get('cert');
+   * @example
+   * $ CERT=/data/keys/cert.pem npm start
+   * @example
+   * $ node app --cert /data/keys/cert.pem
+   */
+  cert: {
+    doc: 'The path to the cert.pem for ssl/tls',
+    default: path.resolve(__dirname, '..', 'server.crt'),
+    env: 'CERT',
+    arg: 'cert'
+  },
+  /**
+   * @member {String} [log='info'] - The lowest log level to log. Possible
+   * values are 'silly', 'debug', 'verbose', 'info', 'warn', 'error'.
+   * @example
+   * config.get('logLevel');
+   * @example
+   * $ LOG=warn npm start
+   * @example
+   * $ node app --log verbose
+   */
+  logLevel: {
+    doc: 'The level at which log messages should be saved',
+    format: ['silly', 'debug', 'verbose', 'info', 'warn', 'error'],
+    default: 'info',
+    env: 'LOG',
+    arg: 'log'
+  },
+  /**
+   * @member {Boolean} [disableConsole=false] - Whether or not to disable
+   * console logging
+   * @example
+   * config.get('disableConsoleLog');
+   * @example
+   * $ DISABLE_CONSOLE=true npm start
+   * @example
+   * $ node app --disable-console
+   */
+  disableConsole: {
+    doc: 'Whether or not to disable console logging',
+    format: Boolean,
+    default: false,
+    env: 'DISABLE_CONSOLE',
+    arg: 'disable-console'
   }
 });
+
+config.load(require('./env.default'));
+config.load(require('./env.' + config.get('env')));
+
+config.validate();
 
 module.exports = config;
--- test/config-test.js
+++ test/config-test.js
@@ -2,12 +2,42 @@
 
 var expect = require('expect.js');
 
+var ENV_VARS = [
+  'NODE_ENV',
+  'DB',
+  'PORT',
+  'KEY',
+  'CERT',
+  'LOG',
+  'DISABLE_CONSOLE'
+];
+
+var origEnv = ENV_VARS.reduce(function (env, envKey) {
+  env[envKey] = process.env[envKey];
+  return env;
+}, {});
+
+function resetEnv() {
+  ENV_VARS.map(function (envKey) {
+    process.env[envKey] = origEnv[envKey];
+  });
+}
+
 function loadConfig() {
-  delete require.cache[require.resolve('../config')];
   return require('../config');
 }
 
+function deleteCache(modulePath) {
+  delete require.cache[require.resolve(modulePath)];
+}
+
 describe('config', function () {
+
+  afterEach(function () {
+    resetEnv();
+    deleteCache('../config');
+    deleteCache('convict');
+  });
 
   it('should load the configuration', function () {
     var config = loadConfig();
@@ -15,7 +45,15 @@ describe('config', function () {
     expect(config.get).to.be.a('function');
   });
 
-  it('should get the required config options');
+  it('should get the required config options with defaults', function () {
+    var config = loadConfig();
+    expect(config.get('env')).to.be('test');
+    expect(config.get('db')).to.be.a('string');
+    expect(config.get('host')).to.be.a('string');
+    expect(config.get('port')).to.be.a('number');
+    expect(config.get('key')).to.be.a('string');
+    expect(config.get('cert')).to.be.a('string');
+  });
 
   it('should verify that the ssl key file exists');
 
