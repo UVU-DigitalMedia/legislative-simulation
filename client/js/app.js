697d39ffede7aa578abde18b2c29bad9afd50093
james <jatoney@hotmail.com>
2015-02-08 00:43:23 -0700

    added basic routes and UI for development ease of use.  added web components and polymer. added to bower...

--- client/js/app.js
+++ client/js/app.js
@@ -5,8 +5,13 @@ var app = angular.module('app', [
     'ngRoute'
 ]);
 
-app.config(['$routeProvider',function(){
-    $routrProvider
-        .when('/',{templateUrl: '', controller:'home'})
-        .otherwise({redirectTo:'/'});
+app.config(['$routeProvider',function($routeProvider){
+    $routeProvider
+        .when('/',{templateUrl: 'client/templates/info.html', controller:'HomeController'})
+        .when('/info',{templateUrl: 'client/templates/info.html', controller:'InfoController'})
+        .when('/info/:val1',{templateUrl: 'client/templates/info.html', controller:'InfoController'})
+        .when('/stage',{templateUrl: 'client/templates/stages.html', controller:'StageController'})
+        .when('/stage/:val1',{templateUrl: 'client/templates/stages.html', controller:'StageController'})
+
+        .otherwise('/');
 }])
---
+++ gulpfile.js
@@ -0,0 +1,1 @@
+//todo add vulcaize to minify and compile polymer
