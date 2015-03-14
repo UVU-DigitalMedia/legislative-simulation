5e16fcfd78b33bf81b3cc426a47d7e4f5870a3ef
james <jatoney@hotmail.com>
2015-03-14 16:07:07 -0600

    styling

--- client/js/app.js
+++ client/js/app.js
@@ -7,9 +7,9 @@ var app = angular.module('app', [
 
 app.config(['$routeProvider',function($routeProvider){
     $routeProvider
-        .when('/',{templateUrl: 'client/templates/info.html', controller:'HomeController'})
-        .when('/intro',{templateUrl: 'client/templates/info.html', controller:'HomeController'})
-        .when('/intro/:val1',{templateUrl: 'client/templates/info.html', controller:'HomeController'})
+        .when('/',{templateUrl: 'client/templates/intro.html', controller:'HomeController'})
+        .when('/intro',{templateUrl: 'client/templates/intro.html', controller:'HomeController'})
+        .when('/intro/:val1',{templateUrl: 'client/templates/intro.html', controller:'HomeController'})
         .when('/info',{templateUrl: 'client/templates/info.html', controller:'InfoController'})
         .when('/info/:val1',{templateUrl: 'client/templates/info.html', controller:'InfoController'})
         .when('/stage',{templateUrl: 'client/templates/stages.html', controller:'StageController'})
