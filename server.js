061861ee7c03fd4d33773680684afbc091b4a7bf
Landon Conover <landon.conover@gmail.com>
2015-04-16 11:17:09 -0600

    New Server Time!

---
+++ server.js
@@ -0,0 +1,13 @@
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+//set the port :)
+var port = process.env.PORT || 9090;
+
+//serve the client out of /client
+app.use(express.static(__dirname + '/client'));
+
+// Start the SERVER of AWESEOME!
+app.listen(port);
+console.log('Magic happens on port ' + port);
